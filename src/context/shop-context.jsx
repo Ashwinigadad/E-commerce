967e64ce79d08b6af22c81e6cc866cf51f7f946e
Ashwinigadad <ashwinigadad3@gmail.com>
2024-06-24 22:38:45 +0530

    initial

---
+++ src/context/shop-context.jsx
@@ -0,0 +1,13 @@
+import React, { createContext } from 'react'
+export const SHopContext = createContext(null);
+
+export const ShopcontextProvider = () => {
+  return (
+    <div>
+        short-context
+      
+    </div>
+  )
+}
+
+
---
+++ src/products.js
@@ -0,0 +1,60 @@
+import product1 from './assets/badam.png'
+import product2 from './assets/carrot.png'
+import product3 from './assets/coconut.png'
+import product4 from './assets/lemon.png'
+import product5 from './assets/masala.png'
+import product6 from './assets/pineapple.png'
+import product7 from './assets/strawberry.png'
+import product8 from './assets/tomato.png'
+
+
+export const PRODUCTS = [
+    {
+      id: 1,
+      productName: "badam",
+      price: 500.0,
+      productImage: product1,
+    },
+    {
+      id: 2,
+      productName: "carrot",
+      price: 500.0,
+      productImage: product2,
+    },
+    {
+      id: 3,
+      productName: "coconut",
+      price: 699.0,
+      productImage: product3,
+    },
+    {
+      id: 4,
+      productName: "lemon",
+      price: 228.0,
+      productImage: product4,
+    },
+    {
+      id: 5,
+      productName: "masala",
+      price: 19.99,
+      productImage: product5,
+    },
+    {
+      id: 6,
+      productName: "pineapple",
+      price: 68.0,
+      productImage: product6,
+    },
+    {
+      id: 7,
+      productName: "strawberry",
+      price: 120.0,
+      productImage: product7,
+    },
+    {
+      id: 8,
+      productName: "tomato",
+      price: 40.0,
+      productImage: product8,
+    },
+  ];
