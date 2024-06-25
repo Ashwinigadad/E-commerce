72875e0ead5441214042c061a54e840ff20f4143
Ashwinigadad <ashwinigadad3@gmail.com>
2024-06-25 22:41:18 +0530

    shop-context

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,13 +1,25 @@
-import React, { createContext } from 'react'
-export const SHopContext = createContext(null);
+import React, { createContext,useState } from 'react'
+export const ShopContext = createContext(null);
 
-export const ShopcontextProvider = () => {
-  return (
-    <div>
-        short-context
-      
-    </div>
-  )
+const getCartDeafault = ()=>{
+  let cart={};
+  for(let i=1;i<PRODUCTS.length+1;i++){
+    cart[i]=0;
+  }
+  return cart;
+}
+
+export const ShopcontextProvider = (props) => {
+  const [cartItems,setcartItems] = useState(getCartDeafault());
+  const addTocart = (itemID)=>{
+    setcartItems((prev)=>({...prev,[itemID]:prev[itemID]+1}));
+  }
+
+  const removeFromcart = (itemID)=>{
+    setcartItems((prev)=>({...prev,[itemID]:prev[itemID]-1}));
+  }
+  return <ShopContext.Provider>{props.children}</ShopContext.Provider>
+
 }
 
 
